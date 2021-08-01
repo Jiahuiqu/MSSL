2c597b79fefea83b510f631f275513a6bce2f92a
Jiahuiqu <78287811+Jiahuiqu@users.noreply.github.com>
2021-08-01 17:33:07 +0800

    Update SAM.py

--- SAM.py
+++ SAM.py
@@ -12,8 +12,7 @@ class SAM(torch.nn.Module):
         data2 = torch.sqrt(torch.sum((output **2),dim = 1) * torch.sum((HS ** 2),dim = 1))
         data2[data2 == 0] = 1e-16
         sam_loss = torch.acos((data1 / data2)).view(-1).mean().type(torch.float32)*180/torch.tensor(np.pi)
-        sam_loss = sam_loss.clone().detach().requires_grad_(True)
-
+        
         return sam_loss
 
 
